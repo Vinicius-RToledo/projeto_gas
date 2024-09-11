ea5e7cb1313f2924975a1f2ea0d932a2a8114527
Lucas Lima Batista <lucaslimabatista.eng@gmail.com>
2024-09-10 21:04:02 -0300

    adição da dinamica da listagem dos produtos e inicio da criação do cart

--- js/localStorage.js
+++ js/localStorage.js
@@ -25,9 +25,14 @@ function getLocalStorage(tabela) {
 function setLocalStorage() {
     // Dados padrão para produtos e clientes
     const produtos = [
-        { id_produto: 1, nome_produto: 'gas', desc_produto: 'botijão de gás de 13kg', quantidade_disponivel: 5, preco_produto: 2, nivel_abastecimento: 3 },
-        { id_produto: 5, nome_produto: 'prego', desc_produto: 'prego fino de aço', quantidade_disponivel: 44, preco_produto: 200, nivel_abastecimento: 4 },
-        { id_produto: 7, nome_produto: 'agua', desc_produto: 'galão de água 20 L', quantidade_disponivel: 14, preco_produto: 10, nivel_abastecimento: 5 }
+        { id_produto: 1, nome_produto: 'gas', desc_produto: 'botijão de gás de 13kg', quantidade_disponivel: 5, preco_produto: 2, nivel_abastecimento: 3, imagem_produto: "/img/gas.jpg" },
+        { id_produto: 5, nome_produto: 'prego', desc_produto: 'prego fino de aço', quantidade_disponivel: 44, preco_produto: 200, nivel_abastecimento: 4, imagem_produto: "/img/prego.jpg"},
+        { id_produto: 7, nome_produto: 'agua', desc_produto: 'galão de água 20 L', quantidade_disponivel: 14, preco_produto: 10, nivel_abastecimento: 5, imagem_produto: "/img/galao.png"},
+        { id_produto: 2, nome_produto: 'agua', desc_produto: 'galão de água 20 L', quantidade_disponivel: 14, preco_produto: 10, nivel_abastecimento: 5, imagem_produto: "/img/galao.png"},
+        { id_produto: 3, nome_produto: 'agua', desc_produto: 'galão de água 20 L', quantidade_disponivel: 14, preco_produto: 10, nivel_abastecimento: 5, imagem_produto: "/img/galao.png"},
+        { id_produto: 4, nome_produto: 'agua', desc_produto: 'galão de água 20 L', quantidade_disponivel: 14, preco_produto: 10, nivel_abastecimento: 5, imagem_produto: "/img/galao.png"}
+
+
     ];
     
     const clientes = [
@@ -36,9 +41,12 @@ function setLocalStorage() {
         { id_cliente: 9, nome_cliente: 'joaa', telefone_cliente: '44433', rua_cliente: 'ffdfd', numero_cliente: '3' }
     ];
 
+    const cart = [];
+
     // Define os dados no localStorage
     localStorage.setItem('produtos', JSON.stringify(produtos));
     localStorage.setItem('clientes', JSON.stringify(clientes));
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 // Chama a função para garantir que os dados estejam no localStorage
